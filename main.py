b2b6217ec95bb3cd70e5b2b31a6b44c19ddcee1d
sheetalrprasad <sheetalraniprasad@gmail.com>
2022-06-10 13:34:46 -0700

    code refining

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 import sys
 import time
 from dataprep.dataPrep import callDataPrep
 from openrefine.openRefine import callOpenRefine
-from createWikidataItems.finalPrepareStep import createWikidataItems
+from createwikidataitems.finalPrepareStep import createWikidataItems
 
 
 def timerCountdownPrint(seconds):
